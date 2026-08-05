88b1e254ce951ca2feacd7764430d3ac0c11d515
Muhammad Hameez <hameez172@gmail.com>
2026-08-06 01:25:01 +0500

    Update database.py

--- database.py
+++ database.py
@@ -459,6 +459,25 @@ def initialize_database():
     )
     """)
 
+    # -----------------------------
+# USERS
+# -----------------------------
+
+    c.execute("""
+CREATE TABLE IF NOT EXISTS users (
+
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    username TEXT UNIQUE NOT NULL,
+
+    password TEXT NOT NULL,
+
+    role TEXT DEFAULT 'user',
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+)
+""")
     conn.commit()
     conn.close()
 
